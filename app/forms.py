6b4b05b318062f5662d9e4bf70a6c401db45d0c2
Sasha Cooper <jinksy@gmail.com>
2018-08-03 15:54:56 +0100

    End of chapter 5 (untested)

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,21 @@ class LoginForm(FlaskForm):
   password = PasswordField('Password', validators=[DataRequired()])
   remember_me = BooleanField('Remember Me')
   submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+  username = StringField('Username', validators=[DataRequired()])
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+  # should that 'password' really be a string?
+  submit = SubmitField('Register')
+
+  def validate_username(self, username):
+    user = User.query.filter_by(username=username.data).first()
+    if user is not None:
+      raise ValidationError('Please use a less sucky username.')
+
+  def validate_email(self, email):
+    user = User.query.filter_by(email=email.data).first()
+    if user is not None:
+      raise ValidationError('Please use a less sucky email.')
